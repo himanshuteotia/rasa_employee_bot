b272641d5c03e1d78f393000f177ec8442636540
himanshuteotia <himanshuteotia7@gmail.com>
2020-04-13 01:43:13 +0530

    updated code according to requirements

--- actions.py
+++ actions.py
@@ -7,21 +7,33 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+employee_details = [{
+    "id_no" : "EMP-234",
+    "name" : "Himanshu Teotia",
+    "status" : "active",
+    "designation": "Chatbot developer"
+},{
+    "employee_id_no" : "EMP-235",
+    "employee_name" : "Suresh",
+    "employee_status" : "deactive",
+    "designation": "Frontend developer"
+}]
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
--- data/nlu.md
+++ data/nlu.md
@@ -1,56 +1,20 @@
 ## intent:greet
 - hey
-- hello
+- hello chatbot
 - hi
 - good morning
 - good evening
 - hey there
 
-## intent:goodbye
-- bye
-- goodbye
-- see you around
-- see you later
+## intent: ask_employee_name
+- Can I have an employee name
+- Can you please please provide employee name
+- employee name please
+- employee name
+- please emplyee name
 
-## intent:affirm
-- yes
-- indeed
-- of course
-- that sounds good
-- correct
+## intent: employee_id
+- Its [EMP-234](employee_id_no)
+- [EMP-234](employee_id_no)
+- My employee id is [EMP-234](employee_id_no)
 
-## intent:deny
-- no
-- never
-- I don't think so
-- don't like that
-- no way
-- not really
-
-## intent:mood_great
-- perfect
-- very good
-- great
-- amazing
-- wonderful
-- I am feeling very good
-- I am great
-- I'm good
-
-## intent:mood_unhappy
-- sad
-- very sad
-- unhappy
-- bad
-- very bad
-- awful
-- terrible
-- not very good
-- extremely sad
-- so sad
-
-## intent:bot_challenge
-- are you a bot?
-- are you a human?
-- am I talking to a bot?
-- am I talking to a human?
